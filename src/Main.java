c01bad82f69d3f9697a58cf7bad0d89f139b2baf
N1S4H <joanna.radom12@gmail.com>
2025-05-15 17:26:17 +0200

    projekt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.time.LocalDate;
-
-public static void main(String[] args) {
-    CountryWithoutProvinces polska = new CountryWithoutProvinces("PL");
-    polska.addDailyStatistic(LocalDate.of(2025, 1, 1), 5, 2);
-    polska.addDailyStatistic(LocalDate.of(2025, 1, 1), 5, 2);
-}
+public class Main{
+    public static void main(String[] args) {
+        CountryWithoutProvinces polska = new CountryWithoutProvinces("PL");
+        polska.addDailyStatistic(LocalDate.of(2025, 1, 1), 5, 2);
+        polska.addDailyStatistic(LocalDate.of(2025, 1, 1), 5, 2);
+    }
 }
